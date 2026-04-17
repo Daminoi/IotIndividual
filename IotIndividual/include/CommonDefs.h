05fd8ca0c0d21308a56feaadcc2644b5a23357b3
Damino <ferioli.damiano@gmail.com>
2026-04-17 19:32:00 +0200

    folder setup

---
+++ IotIndividual/include/CommonDefs.h
@@ -0,0 +1,13 @@
+#ifndef COMMONDEFS_H
+#define COMMONDEFS_H
+
+// uncomment the following definition if the program should communicate via LORA + TTN
+#define COMMS_VIA_LORA_TTN 1
+
+// uncomment the following definition if the program should communicate via WIFI + MQTT
+// #define COMMS_VIA_WIFI_MQTT 1
+
+// comment the following line to disable debugging via LEDs and/or serial
+#define DEBUG_ACTIVE 1
+
+#endif /* COMMONDEFS_H */
